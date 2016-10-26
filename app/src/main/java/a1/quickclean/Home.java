40c1cc9c608759325ff49867e8637bbb2cc0279d
lizkund <lizkund@gmail.com>
2016-10-25 21:29:29 -0700

    Initial commit

---
+++ app/src/main/java/a1/quickclean/Home.java
@@ -0,0 +1,7 @@
+package a1.quickclean;
+
+/**
+ * Created by Liz on 13/10/2016.
+ */
+public class Home {
+}
---
+++ app/src/main/java/a1/quickclean/SQLite/dbSched.java
@@ -0,0 +1,7 @@
+package a1.quickclean.SQLite;
+
+/**
+ * Created by Liz on 25/10/2016.
+ */
+public class dbSched {
+}
